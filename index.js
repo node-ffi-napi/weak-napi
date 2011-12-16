3261f1cbd4694c074726c1680f7add7bc15b782a
Nathan Rajlich <nathan@tootallnate.net>
2011-12-15 21:31:02 -0800

    Windows support. Closes #1.
    Squashed commit of the following:
    
    commit 8d807f9fdf668019480ec85fa7a1a5704f05a730
    Author: Nathan Rajlich <nathan@tootallnate.net>
    Date:   Thu Dec 15 21:28:26 2011 -0800
    
        Ignore node-waf build files.
    
    commit 761793ef362425315a9381c18ab575f80a2e8461
    Author: Nathan Rajlich <nathan@tootallnate.net>
    Date:   Thu Dec 15 21:23:05 2011 -0800
    
        Attempt to load platform-specific precompiled bindings first.
    
    commit f7a919f54605164c89a68481fecfb15dd2e69b6e
    Author: Nathan Rajlich <nathan@tootallnate.net>
    Date:   Thu Dec 15 21:22:38 2011 -0800
    
        Actually, put the binary in it's own dir.
    
        We have to retain the original name "weakref.node".
    
    commit 3c89af987d7c051a4d9a0ed77b137cfe509c7f7b
    Author: unknown <Nathan@Nathan-PC.(none)>
    Date:   Thu Dec 15 21:14:57 2011 -0800
    
        Add a Windows binary.
    
    commit ec9f5f171e3e65f23848a097d48b8842cac406b4
    Author: Nathan Rajlich <nathan@tootallnate.net>
    Date:   Thu Dec 15 20:50:44 2011 -0800
    
        Add module.gyp and node_module.gypi files.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var bindings
+
 try {
+  bindings = require('./build/'+process.platform+'/'+process.arch+'/weakref')
+} catch (e) { try {
   bindings = require('./build/Release/weakref')
 } catch (e) { try {
   bindings = require('./build/default/weakref')
 } catch (e) {
   throw e
-}}
+}}}
 
 module.exports = bindings.create
 
---
+++ module.gyp
@@ -0,0 +1,19 @@
+{
+  'variables': {
+    'module_name': 'weakref',
+  },
+  'targets': [
+    {
+      'sources': [
+        'src/weakref.cc',
+      ],
+      'include_dirs': [
+      ],
+      'defines': [
+      ],
+      'includes': [
+        'node_module.gypi',
+      ]
+    }
+  ]
+}
